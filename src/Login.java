92db90001d925458ddc3a8cf7ed07df6e92cef45
EDiaz210 <elkin.diaz@epn.du.ec>
2025-01-16 08:57:45 -0500

    login fiish

--- src/Login.java
+++ src/Login.java
@@ -15,8 +15,7 @@ public class Login {
         logInButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (Seleccionador.getSelectedItem().toString().equals("Administrador")){
-                    System.out.println("Ingresaste a modo administrador ");
+
 
                     String url = "jdbc:mysql://ul1p0vkekpygav1f:g4TMqDpYcFoAkQKd4tWx@bjaibh7r281ex1wojndt-mysql.services.clever-cloud.com:3306/bjaibh7r281ex1wojndt";
                     String usuario = "ul1p0vkekpygav1f";
@@ -24,60 +23,30 @@ public class Login {
 
                     try (Connection connection = DriverManager.getConnection(url, usuario, password)) {
                         Statement statement = connection.createStatement();
-                        String query = "SELECT * FROM usuarios WHERE username = '" + textField1.getText() + "' AND password = '" + new String(passwordField1.getPassword()) + "'";
+                        String query = "SELECT * FROM usuarios WHERE username = '" + textField1.getText() + "' AND password = '" + new String(passwordField1.getPassword()) + "' AND rol = '" + Seleccionador.getSelectedItem().toString() + "'";
                         ResultSet resultSet = statement.executeQuery(query);
 
 
                         if(resultSet.next()) {
-                            if(textField1.getText().equals(resultSet.getString("username")) && new String(passwordField1.getPassword()).equals(resultSet.getString("password"))) {
-                                JFrame frame = new JFrame();
-                                frame.setTitle("Login");
-                                frame.setSize(350, 200);
-                                frame.setContentPane(new MenuAdmin().menuAd);
-                                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                                frame.setVisible(true);
+                            if (Seleccionador.getSelectedItem().equals(resultSet.getString("rol"))){
+                                System.out.println("Ingresaste a modo administrador ");
+                                if(textField1.getText().equals(resultSet.getString("username")) && new String(passwordField1.getPassword()).equals(resultSet.getString("password"))) {
+                                    JFrame frame = new JFrame();
+                                    frame.setTitle("Login");
+                                    frame.setSize(350, 200);
+                                    frame.setContentPane(new MenuAdmin().menuAd);
+                                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                    frame.setVisible(true);
 
-                                ((JFrame) SwingUtilities.getWindowAncestor(logInButton)).dispose();
-                            }
-                        }else {
+                                    ((JFrame) SwingUtilities.getWindowAncestor(logInButton)).dispose();
+                                }
+                            }else {
                             resultado.setText("Digite correctamente las credenciales");
                         }
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
-                    }
-
-
-
-
-                }else if (Seleccionador.getSelectedItem().toString().equals("Usuario")){
-                    System.out.println("Ingresaste a modo usuario");
-
-                    String url = "jdbc:mysql://ul1p0vkekpygav1f:g4TMqDpYcFoAkQKd4tWx@bjaibh7r281ex1wojndt-mysql.services.clever-cloud.com:3306/bjaibh7r281ex1wojndt";
-                    String usuario = "ul1p0vkekpygav1f";
-                    String password = "g4TMqDpYcFoAkQKd4tWx";
-
-                    try (Connection connection = DriverManager.getConnection(url, usuario, password)) {
-                        Statement statement = connection.createStatement();
-                        String query = "SELECT * FROM usuarios WHERE username = '" + textField1.getText() + "' AND password = '" + new String(passwordField1.getPassword()) + "'";
-                        ResultSet resultSet = statement.executeQuery(query);
-
-
-                        if(resultSet.next()) {
-                            if(textField1.getText().equals(resultSet.getString("username")) && new String(passwordField1.getPassword()).equals(resultSet.getString("password"))) {
-                                JFrame frame = new JFrame();
-                                frame.setTitle("Login");
-                                frame.setSize(350, 200);
-                                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                                frame.setVisible(true);
-
-                                ((JFrame) SwingUtilities.getWindowAncestor(logInButton)).dispose();
-                            }
-                        }else {
-                            resultado.setText("Digite correctamente las credenciales");
                         }
                     } catch (SQLException ex) {
-                        ex.printStackTrace();
-                    }
+                        throw new RuntimeException(ex);
+
                 }
             }
         });
