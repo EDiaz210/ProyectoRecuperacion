84bd0aff4e6cd9bb8bb30e9353ce445fb69ec653
POO <orlandosara@outlook.es>
2025-01-16 08:41:52 -0500

    Creacion de la interfaz visual del menu de administradores

--- src/MenuAdmin.java
+++ src/MenuAdmin.java
@@ -2,4 +2,15 @@ import javax.swing.*;
 
 public class MenuAdmin {
     public JPanel menuAd;
+    private JTabbedPane tabbedPane1;
+    private JTextField nombre_p;
+    private JTextField cantidad_p;
+    private JTextField precio_p;
+    private JButton Agregar_p;
+    private JTextField new_user;
+    private JPasswordField new_pass;
+    private JButton insert_user;
+    private JTable table1;
+    private JTable table2;
+    private JTextField codigo_p;
 }
