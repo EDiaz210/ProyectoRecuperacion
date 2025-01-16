915cb7df73f105c5b668ffa19632ec63bd1971ac
POO <orlandosara@outlook.es>
2025-01-16 08:57:50 -0500

    Creacion completa del menu de adiministrador

--- src/MenuAdmin.java
+++ src/MenuAdmin.java
@@ -3,6 +3,15 @@ import javax.swing.*;
 public class MenuAdmin {
     public JPanel menuAd;
     private JTabbedPane tabbedPane1;
+    private JButton Agregar_P;
+    private JTextField codigo_p;
+    private JTextField nombre_p;
+    private JTextField cantidad_p;
+    private JTextField preciou_p;
+    private JTextField new_user;
+    private JTextField new_pass;
+    private JButton Ingresar_user;
 
     //ffr
+    //Creacion total
 }
