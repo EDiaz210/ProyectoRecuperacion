4ef28f4a5c82654ce1e54c4e3540efce89f486e2
Orlan <orlandosara@outlook.es>
2025-01-16 21:02:06 -0500

    Agregar productos a la tabla

--- src/MenuAdmin.java
+++ src/MenuAdmin.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 
 public class MenuAdmin {
     public JPanel menuAd;
@@ -17,6 +18,7 @@ public class MenuAdmin {
     private JTextField new_user;
     private JTextField new_pass;
     private JButton Ingresar_user;
+    private JTextField descrip;
 
     //ffr
     //Creacion total
@@ -27,25 +29,36 @@ public class MenuAdmin {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                String query = "INSERT INTO productos (id, nombre, descripcion ,precio, stock ) VALUES (?, ?, ?, ?, ?)";
+
                 String id = codigo_p.getText();
+                String nombre = nombre_p.getText();
+                String descripcion = descrip.getText();
+                String precio = preciou_p.getText();
+                String cantidad = cantidad_p.getText();
+
+                String query = "INSERT INTO productos (id, nombre, descripcion ,precio, stock ) VALUES (?, ?, ?, ?, ?)";
+
+                int cantidara_int=Integer.parseInt(cantidad);
+                if (cantidara_int >= 10 && cantidara_int <= 350) {
+                    try (Connection connection = usuarios.ConexionBD.getConnection()) {
+                        PreparedStatement pstmt = connection.prepareStatement(query);
+
+                        pstmt.setInt(1, Integer.parseInt(id));
+                        pstmt.setString(2, nombre);
+                        pstmt.setString(3, descripcion);
+                        pstmt.setDouble(4, Double.parseDouble(precio));
+                        pstmt.setInt(5, Integer.parseInt(cantidad));
+
+                        pstmt.executeUpdate();
 
 
-                try(Connection connection = usuarios.ConexionBD.getConnection()){
-                    PreparedStatement cadenaPreparada = connection.prepareStatement(query);
-
-
-
-
-
-
-
-
-            } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
+                    } catch (SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(menuAd,"VALOR DE STOCK INVALIDO / REINTENTE");
                 }
             }
-            });
-
+        });
     }
 }
